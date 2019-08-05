2c267cde4ee1c24984a70cb05f3bc87a7d5fc2a3
Antje Sommer <antje.som@gmail.com>
2019-08-05 23:00:16 +0200

    update product review component

--- app.js
+++ app.js
@@ -11,10 +11,52 @@ Vue.component('product-details', {
 })
 
 Vue.component('product-review', {
-    template: `<input v-model="name">`,
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name" placeholder="name" >
+            </p>
+
+            <p>
+                 <label for="review">Review:</label>
+                 <textarea id="review" v-model="review"></textarea>
+            </p>
+
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    `,
     data() {
         return {
-            name: null
+            name: null,
+            review: null,
+            rating: null,
+        }
+    },   
+    methods: {
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
         }
     }
 })
@@ -42,10 +84,21 @@ Vue.component('vc-product', {
                 <product-details :details="details"></product-details>
                 <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box" :style="{backgroundColor: variant.variantColor}" @mouseover="updateProduct(index)" @mouseleave="mouseleave">
                 </div>
-                <product-review></product-review>
                 <button v-on:click="addToCart " :disabled="!inStock " :class="{disabledButton: !inStock} ">Add to cart</button>
-                <button v-on:click="clearCart">Clear Cart</button
+                <button v-on:click="clearCart">Clear Cart</button>
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <p>{{ review.name }}</p>
+                            <p>Rating: {{ review.rating }} </p>
+                            <p>{{ review.review }}</p>
+                        </li>
+                    </ul>
                 </div>
+                <product-review @review-submitted="addReview"></product-review>
+            </div>
         </div>
     `,
     data() {
@@ -54,6 +107,7 @@ Vue.component('vc-product', {
             product: 'Socks',
             selectedVariant: 0,
             altText: 'a pair of socks',
+            reviews: [],
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [{
                     variantID: 2234,
@@ -85,6 +139,9 @@ Vue.component('vc-product', {
         },
         mouseleave() {
             this.selectedVariant = 0
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
